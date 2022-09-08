b60dd72f299021c5614c51b1056c5d5ce55c4dc2
Quin-Riley-A <quin.asselin1@gmail.com>
2022-09-07 23:40:05 -0400

    handle further q corner case behavior needs edit Co-authored-by: Emil Anton <emilbstn@gmail.com> Co-authored-by: Noah Cowan <nsncowan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -17,10 +17,12 @@ function pigLatinConsonant(word) {
     }
     word = word.slice(index, word.length);
     if (slicedLetters[0] === "q") {
-      slicedLetters.push(word.charAt(index));
+      slicedLetters.push(word.charAt(0));
       word = word.slice(1,word.length);
     } else if (slicedLetters[-1] === "q" && slicedLetters[0] !== "q") {
-      
+      slicedLetters.pop();
+      word = "q" + word + slicedLetters.join("")+"ay";
+      return word;
     }
 
     return word.concat(slicedLetters.join("")+ "ay"); 
