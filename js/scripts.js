4b82be96940eae2a5f549b33164392ae0eb1a95c
Quin-Riley-A <quin.asselin1@gmail.com>
2022-09-07 22:25:22 -0400

    built word starting with consonant behavior Co-authored-by: Emil Anton <emilbstn@gmail.com> Co-authored-by: Noah Cowan <nsncowan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,22 @@ function pigLatinVowel(word) {
   if (vowels.includes(word.charAt(0)))
   return word.concat("way");
 }
+
+function pigLatinConsonant(word) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let n = true;
+  let index = 0;
+  let slicedLetters = [];
+  while (n) {
+    if (!(vowels.includes(word.charAt(index)))) {
+      slicedLetters.push(word.charAt(index));
+      index += 1;
+      continue;
+    }
+    word = word.slice(index, word.length);
+    return word.concat(slicedLetters.join("")+ "ay");
+    
+  }
+}
+
+
